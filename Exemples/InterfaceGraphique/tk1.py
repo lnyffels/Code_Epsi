c69a1b9ddbca4a589e729bd51738450013790549
lnyffels <lnyffels@hotmail.com>
2019-10-24 16:59:14 +0200

    add feat("tkinter")

---
+++ Exemples/InterfaceGraphique/tk1.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+
+racine = tk.Tk()
+label = tk.Label(racine, text="J'adore Python !")
+zone = tk.Entry(racine)
+
+def change_text():
+    s = zone.get()
+    label["text"] = s
+
+
+boutonQuit = tk.Button(racine,text ="Quitter ", fg="red", command=racine.destroy)
+boutonModifyText = tk.Button(racine,text ="Modifier texte", fg="Blue", command=change_text)
+
+label.pack()
+zone.pack()
+boutonQuit.pack()
+boutonModifyText.pack()
+
+racine.mainloop()
+
+
+
+
+
+
